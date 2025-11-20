e8ba7c6bd767716471c4c559d846d1bb12de9585
Tobi <33387497+Tobiscuit@users.noreply.github.com>
2025-11-19 20:09:30 -0600

    Fix corrupted OrdersClient file

--- src/components/orders/orders-client.tsx
+++ src/components/orders/orders-client.tsx
@@ -13,21 +13,6 @@ import {
   TableCell,
   TableHead,
   TableHeader,
-'use client';
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
   TableRow,
 } from '@/components/ui/table';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
