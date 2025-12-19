58680656fd3e445f14e180fdbe2fcf8d8e2d40f7
Tobi <33387497+Tobiscuit@users.noreply.github.com>
2025-12-18 19:14:35 -0600

    fix: Add missing cn import to image-studio

--- src/components/image-studio/index.tsx
+++ src/components/image-studio/index.tsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 import {
   Form,
   FormControl,
